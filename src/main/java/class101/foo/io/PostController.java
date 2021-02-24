c750baa62b3678bc07db8c794fa463081c214b02
snepbnt <marketerchoi@gmail.com>
2021-02-24 22:58:40 +0900

    feat: db에서 es를 사용하도록 변경

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -3,10 +3,6 @@ package class101.foo.io;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,9 +21,6 @@ public class PostController {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Autowired
-    PostCacheService postCacheService;
-
     // 1. 글을 작성한다.
     @PostMapping("/post")
     public Post createPost(@RequestBody Post post) throws JsonProcessingException {
@@ -35,28 +28,6 @@ public class PostController {
         producer.sendTo(jsonPost);
         return post;
     }
-
-
-    // 2-1 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        if(page.equals(1)){
-            return postCacheService.getFirstPostPage();
-        } else{
-            return postRepository.findAll(
-                    PageRequest.of(page -1, PAGE_SIZE, Sort.by("id").descending())
-            );
-        }
-
-
-    }
-
-    // 3. 글 번호로 조회
-    @GetMapping("/posts/{id}")
-    public Post getPostById(@PathVariable("id") Long id) {
-        return postRepository.findById(id).get();
-    }
-
 
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
     @GetMapping("/search")
